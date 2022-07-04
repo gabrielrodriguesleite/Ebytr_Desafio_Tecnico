2abe78e97332da02d1333b66722935d97808ccea
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-07-04 20:05:21 -0300

    [new] pagina 404

---
+++ app/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import '../App.css';
+
+export default function NotFound() {
+  return (
+    <div className="App">
+      <main className="App-main">
+        <p>Error 404:</p>
+        <p><b>Page Not Found</b></p>
+      </main>
+    </div>
+  );
+}
